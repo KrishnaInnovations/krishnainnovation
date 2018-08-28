fb5bdb90a3918c4edf8186a66d0e0876bc23dffd
rrana2017 <rinkeshrana1@gmail.com>
2018-08-27 21:20:05 -0400

    Updates

---
+++ WebContent/WebSite.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+function data (){
+	org = {name:"Welcome to Krishna Innovation"};
+	company_name = org.name.bold();
+	company_name = org.name.fontsize("24");
+	return company_name
+}
+
