38518becf39396493fcf22663df3c2a80e72d857
rrana2017 <rinkeshrana1@gmail.com>
2018-09-19 22:07:29 -0400

    5_completed

---
+++ MainPage/js/main01.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+function address_url(){
+	var address_url = "https://www.google.com/maps/place/43,+New+Ramdev+Nagar,+Aanjada+Nagar,+Bhatena,+Surat,+Gujarat+395002,+India/@21.1788649,72.8422237,18z/data=!3m1!4b1!4m5!3m4!1s0x3be04e49cfdc30f3:0x7fc3dacef7757e96!8m2!3d21.1788649!4d72.843318"
+	return address_url;
+}
